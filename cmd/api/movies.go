dabc42d11acdbcc62fbedf20580433c810505042
Allan Fuentes <allanfuenpi@criptext.com>
2023-02-02 21:50:51 -0500

    adding some validators to the input field when POST method is being sent

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/allanfp09/greenlight/internal/data"
+	valdator "github.com/allanfp09/greenlight/internal/validator"
 	"net/http"
 	"time"
 )
@@ -40,6 +41,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := valdator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
